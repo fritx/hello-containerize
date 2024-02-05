4cd11a2a4c3f037ccdeb6ada84bac8bc6464554c
Fritz Lin <uxfritz@163.com>
2024-02-05 13:14:48 +0800

    wip: fix `go get -d` no longer supported outside a module, use `go install` instead

--- script/buildVersionedExecutable.sh
+++ script/buildVersionedExecutable.sh
@@ -7,9 +7,16 @@ function buildVersionedExecutable {
   local package=github.com/${githubUser}/${pgmName}
   local newPgm=$tmpDir/${pgmName}_${version}
 
-  GOPATH=$tmpDir go get -d $package
+  # go: go.mod file not found in current directory or any parent directory.
+  #         'go get' is no longer supported outside a module.
+  #         To build and install a command, use 'go install' with a version,
+  #         like 'go install example.com/cmd@latest'
+  #         For more information, see https://golang.org/doc/go-get-install-deprecation
+  #         or run 'go help get' or 'go help install'.
+  # GOPATH=$tmpDir go get -d $package
+  GOPATH=$tmpDir go install $package@latest
 
-  cat $tmpDir/src/${package}/${pgmName}.go |\
+  cat $tmpDir/pkg/mod/${package}@v*/${pgmName}.go |\
       sed 's/version = 0/version = '${version}'/' \
       >${newPgm}.go
 
